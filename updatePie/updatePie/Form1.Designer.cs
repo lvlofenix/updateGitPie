379f684ee650178f6257fed07f8187e648cdc41f
Lucas Vinicius <fenix@hotmail.com.br>
2015-09-17 16:25:05 -0300

    0.1 Beta
    First!

---
+++ updatePie/updatePie/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace updatePie
+{
+    partial class formUpie
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pb_load = new System.Windows.Forms.ProgressBar();
+            this.lb_log = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // pb_load
+            // 
+            this.pb_load.Location = new System.Drawing.Point(12, 12);
+            this.pb_load.Name = "pb_load";
+            this.pb_load.Size = new System.Drawing.Size(539, 23);
+            this.pb_load.TabIndex = 0;
+            // 
+            // lb_log
+            // 
+            this.lb_log.AutoSize = true;
+            this.lb_log.Location = new System.Drawing.Point(11, 38);
+            this.lb_log.Name = "lb_log";
+            this.lb_log.Size = new System.Drawing.Size(92, 13);
+            this.lb_log.TabIndex = 1;
+            this.lb_log.Text = "Vamos começar...";
+            // 
+            // formUpie
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(563, 57);
+            this.ControlBox = false;
+            this.Controls.Add(this.lb_log);
+            this.Controls.Add(this.pb_load);
+            this.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "formUpie";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Update";
+            this.Load += new System.EventHandler(this.formUpie_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar pb_load;
+        private System.Windows.Forms.Label lb_log;
+    }
+}
+
---
+++ updatePie/updatePie/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace updatePie
+{
+    public partial class formUpie : Form
+    {
+        public string myLocation = Application.StartupPath, upLocation = String.Empty;
+        public formUpie()
+        {
+            InitializeComponent();
+        }
+
+        private void formUpie_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                if (routerRly())
+                {
+                    //se o gitPie estiver aberto, vamos fecha-lo para atualizar.
+                    byePie();
+
+                    //iniciando movimentação.
+                    moveOn();
+                }
+                else
+                {
+                    this.Close();   
+                }
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show (err+"", "Houston, we have a problem...",MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private Boolean routerRly()
+        {
+            string[] args = Environment.GetCommandLineArgs();
+            foreach (string arg in args)
+            {
+                if (!(arg.Contains(@"\") || arg.Contains("/")))
+                    upLocation = arg.ToString();
+            }
+            DirectoryInfo dir = new DirectoryInfo(Path.GetTempPath() + upLocation);
+            if (upLocation.Length >0 & dir.Exists)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        
+        private void moveOn()
+        {
+            DirectoryInfo upDir = new DirectoryInfo(Path.GetTempPath() + upLocation);
+            FileInfo[] upAr = upDir.GetFiles("*.*");
+            pb_load.Maximum = upAr.Length;
+            pb_load.Step = 1;
+            foreach (FileInfo fileinfo in upAr)
+            {
+                if (File.Exists(myLocation + @"\" + fileinfo.Name)) File.Delete(myLocation + @"\" + fileinfo.Name);
+                File.Move(Path.GetTempPath() + upLocation+ @"\" +fileinfo.Name, myLocation + @"\" + fileinfo.Name);
+                pb_load.Increment(1);
+                    
+            }
+        }
+
+        private void byePie()
+        {
+            if (Process.GetProcessesByName("getPie.exe").Length > 1)
+            {
+                lb_log.Text = "Fechando GitPie...";
+                Process[] processes = Process.GetProcessesByName("getPie.exe");
+                foreach (Process process in processes)
+                {
+                    process.Kill();
+                }
+            }
+        }
+
+    }
+}
